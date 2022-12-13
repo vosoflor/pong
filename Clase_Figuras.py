b589992deca139db6d18e0809f0e80bbfb8c4260
Verónica Osorio Florián <vero_oso11@hotmail.com>
2022-12-12 22:28:49 -0500

    Incluye movimiento limitado raquetas y pelota además de la impresión de marcadores

--- Clase_Figuras.py
+++ Clase_Figuras.py
@@ -9,9 +9,24 @@ class Pelota:
         self.color = color
         self.vx = vx
         self.vy = vy
+        self.contador_derecha = 0
+        self.contador_izquierda = 0
     
     def dibujar(self, pantalla):
         pg.draw.circle(pantalla, self.color, (self.pos_x, self.pos_y), self.radio)
+
+    def mover(self, altura_pantalla, ancho_pantalla):
+        self.pos_x += self.vx
+        self.pos_y += self.vy
+        
+        if self.pos_x < (0 + self.radio):
+            self.vx *= -1
+            self.contador_izquierda += 1
+        if self.pos_x >= ancho_pantalla - self.radio:
+            self.vx *= -1
+            self.contador_derecha += 1
+        if self.pos_y < (0 + self.radio) or self.pos_y >= altura_pantalla - self.radio:
+            self.vy *= -1
 
 class Raqueta:
     
@@ -26,3 +41,10 @@ class Raqueta:
     
     def dibujar(self, pantalla):
         pg.draw.rect(pantalla, self.color, (self.pos_x, self.pos_y, self.w, self.h))
+
+    def mover(self, tecla_arriba, tecla_abajo, altura_pantalla):
+
+        if pg.key.get_pressed()[tecla_arriba] and self.pos_y > 0:
+            self.pos_y -= self.vy
+        if pg.key.get_pressed()[tecla_abajo] and self.pos_y < altura_pantalla - self.h:
+            self.pos_y += self.vy
--- main.py
+++ main.py
@@ -8,6 +8,9 @@ pg.init()
 pantalla_principal = pg.display.set_mode((800,600))
 pg.display.set_caption("Pong :D !!!")
 
+# Definir tasa de refresco de nuestro bucle de fotogramas (fotograma por segundo = FPS)
+cronometro = pg.time.Clock()
+
 # Para crear pelota y raquetas con parámetros mínimos requeridos;
 # de los otros parámetros toma los valores por default
 pelota = Clase_Figuras.Pelota(pantalla_principal.get_width()/2, pantalla_principal.get_height()/2)
@@ -19,27 +22,35 @@ game_over = False
 
 while not game_over:
     
+    # La función tick permite conocer los milisegundos que tarda entre cada fotograma
+    # Cuando se incluye parámetro se está definiendo la cantidad máxima de fotogramas a imprimir en un segundo
+    milisegundos = cronometro.tick(300)
+
     for evento in pg.event.get():
         if evento.type == pg.QUIT:
             game_over = True
     
-    if pg.key.get_pressed()[pg.K_DOWN]:
-        raqueta1.pos_y += 1
-        raqueta2.pos_y += 1
-    if pg.key.get_pressed()[pg.K_UP]:
-        raqueta1.pos_y -= 1
-        raqueta2.pos_y -= 1
-    
+    # Método para mover raqueta si las teclas definidas están presionadas
+    raqueta1.mover(pg.K_w, pg.K_s, pantalla_principal.get_height())
+    raqueta2.mover(pg.K_UP, pg.K_DOWN, pantalla_principal.get_height())
+
+    # Método para mover pelota y que rebote dentro del campo de juego
+    pelota.mover(pantalla_principal.get_height(), pantalla_principal.get_width())
+
+    # Métodos para crear superficie de marcadores para cada jugador
+    marcador1 = pg.font.Font(None, 100).render(str(pelota.contador_derecha), 1, (255,255,255))
+    marcador2 = pg.font.Font(None, 100).render(str(pelota.contador_izquierda), 1, (255,255,255))
+
     # Método para asignar color a la pantalla y línea del medio
     pantalla_principal.fill((0, 128, 94))
     pg.draw.line(pantalla_principal, (255,255,255), (400,0), (400,600), 2)
     
-    # Método para dibujar pelota de juego y raquetas
+    # Método para crear pelota de juego y raquetas
     pelota.dibujar(pantalla_principal)
     raqueta1.dibujar(pantalla_principal)
     raqueta2.dibujar(pantalla_principal)
 
     # Método para dibujar y mostrar lo parametrizado anteriormente
+    pantalla_principal.blit(marcador1, (150,10))
+    pantalla_principal.blit(marcador2, (550,10))
     pg.display.flip()
-
-
